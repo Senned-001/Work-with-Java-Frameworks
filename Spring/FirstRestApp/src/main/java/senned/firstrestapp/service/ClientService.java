32b9192fff07fb1e6ebbf969fc5fca0269f4e5a3
Senned-001 <senned.mixed.by@gmail.com>
2020-02-14 17:01:42 +0300

    First commit

---
+++ Spring/FirstRestApp/src/main/java/senned/firstrestapp/service/ClientService.java
@@ -0,0 +1,33 @@
+package senned.firstrestapp.service;
+
+import senned.firstrestapp.model.Client;
+
+import java.util.List;
+
+public interface ClientService {
+
+    /**
+     * Create new client
+     */
+    void create(Client client);
+
+    /**
+     * Return list of all clients
+     */
+    List<Client> readAll();
+
+    /**
+     * Return client with id
+     */
+    Client read(int id);
+
+    /**
+     Update client with id
+     */
+    boolean update(Client client, int id);
+
+    /**
+     Delete client with id
+     */
+    boolean delete(int id);
+}
