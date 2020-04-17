869f8d53a5ef0f2f8c8b92731aa091b21ad2e00e
Senned-001 <senned.mixed.by@gmail.com>
2020-04-17 15:40:49 +0300

    First commit

--- Spring/FirstSpringBootMVCApp/src/main/java/senned/firstspringbootmvc/FirstSpringBootMvcAppApplication.java
+++ Spring/FirstSpringBootMVCApp/src/main/java/senned/firstspringbootmvc/FirstSpringBootMvcAppApplication.java
@@ -3,6 +3,11 @@ package senned.firstspringbootmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Simple MVC WebApp on SpringBoot:
+ * authentification with HTTP-requests
+ */
+
 @SpringBootApplication
 public class FirstSpringBootMvcAppApplication {
 
